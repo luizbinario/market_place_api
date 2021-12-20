b4ce97dc4bcf81cfe0701608a2b53596625906bb
Luiz Carlos <luiz1987@gmail.com>
2021-12-19 21:00:48 -0300

    Create a seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Product.delete_all
+# user = User.create! email: 'corykenshin@mplace.com', password: 'candymine'
+# puts "Created a new user: #{user.email}"
+#
+# Usando a gem faker
+# Gerando Users
+# 5.times do
+#   user = User.create! email: Faker::Internet.email, password: 'candymine'
+#   puts "Created a new user: #{user.email}"
+# end
+
+#Gerando Users & Products
+3.times do
+  user = User.create! email: Faker::Internet.email, password: 'candymine'
+  puts "Created a new user: #{user.email}"
+
+  2.times do
+    product = Product.create!(
+      title: Faker::Commerce.product_name,
+      price: rand(1.0..100.0),
+      published: true,
+      user_id: user.id
+    )
+    puts "Created a brand new product: #{product.title}"
+  end
+end
