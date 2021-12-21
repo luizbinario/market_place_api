35c7652824e566fb10285aad626e22f7b8d61514
Luiz Carlos <luiz1987@gmail.com>
2021-12-21 13:38:52 -0300

    Adds search scopes on the product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('controller').count
+  end
+
+  test "should filter products by name and sort them" do
+    assert_equal [products(:another_controller), products(:one)], Product.filter_by_title('controller').sort
+  end
+
+  test "should filter products by price and sort them" do
+    assert_equal [products(:two), products(:one)], Product.above_or_equal_to_price(200).sort
+  end
+
+  test "should filter products by price lower and sort them" do
+    assert_equal [products(:another_controller)], Product.below_or_equal_to_price(200).sort
+  end
+
+  test "should sort product by the most recent" do
+    products(:two).touch
+    assert_equal [products(:another_controller), products(:one), products(:two)], Product.recent.to_a
+  end
 end
