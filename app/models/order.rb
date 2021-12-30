90c3ed1f0bbf242663f1ca2953b6f7e68c7b27aa
Luiz Carlos <luiz1987@gmail.com>
2021-12-30 00:46:04 -0300

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations 
   before_validation :set_total!
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
+  validates_with EnoughProductsValidator
 
   def set_total!
     # self.total = products.sum :price
